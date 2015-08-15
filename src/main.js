c21387c67d9f691924719f790553e4cfd4344e0d
Roman Liutikov <roman01la@romanliutikov.com>
2015-08-15 10:40:07 +0300

    static layout

--- src/main.js
+++ src/main.js
@@ -2,21 +2,15 @@
 
   'use strict';
 
-  var canvas, canvasStyle;
   var viewport = document.querySelector('.viewport');
   var editorContainer = document.querySelector('.editor');
 
-  var WIDTH = window.innerWidth;
-  var HEIGHT = window.innerHeight;
+  var WIDTH = scope.innerWidth;
+  var HEIGHT = scope.innerHeight;
 
   var STORAGE = 'THREE_JS_CLJS_STORAGE';
 
-  var DEFAULT_DEFS = `
-    (def THREE (.-THREE js/window))
-    (def VIEWPORT (.querySelector js.document ".viewport"))
-    (def WIDTH ${viewport.clientWidth})
-    (def HEIGHT ${viewport.clientHeight})
-    `;
+  var DEFAULT_DEFS = "(def THREE (.-THREE js/window))\n(def VIEWPORT (.querySelector js.document \".viewport\"))\n(def WIDTH " + viewport.clientWidth + ")\n(def HEIGHT " + viewport.clientHeight + ")";
 
   var editor = CodeMirror(editorContainer, {
     value: localStorage.getItem(STORAGE) || getDefaultValue(),
@@ -31,14 +25,9 @@
 
   editor.setOption('extraKeys', {
     'Alt-Enter': function(instance){
-      evalCljs(
-        `(def WIDTH ${viewport.clientWidth})
-        (def HEIGHT ${viewport.clientHeight})`, null,
-        function() {
-          evalCljs(instance.getValue(), function beforeEvalAfterCompile() {
-            viewport.innerHTML = '';
-          });
-        });
+      evalCljs(instance.getValue(), function beforeEvalAfterCompile() {
+        viewport.innerHTML = '';
+      });
     }
   });
 
@@ -46,115 +35,24 @@
     localStorage.setItem(STORAGE, instance.getValue());
   });
 
-  appendResizer(editorContainer, viewport);
-
-  window.addEventListener('resize', onResize, false);
-
   evalCljs(DEFAULT_DEFS);
   evalCljs(editor.getValue());
 
-  function evalCljs(code, callback, cb) {
+  function evalCljs(code, preEvalCallback) {
 
     jsbin_cljs.core.eval_expr(code, function(err, output) {
       if (err) {
         console.error(err);
       } else {
-        callback && callback();
+        preEvalCallback && preEvalCallback();
         eval(output);
-        cb && cb();
       }
     });
   }
 
-  function onResize() {
-
-    WIDTH = window.innerWidth;
-    HEIGHT = window.innerHeight;
-
-    resizeViewport(WIDTH, HEIGHT);
-  }
-
-  function resizeViewport(w, h) {
-
-    if (!canvas) {
-      canvas = viewport.querySelector('canvas');
-      canvasStyle = canvas.style;
-    }
-
-    var ratio = canvas.width / parseFloat(canvasStyle.width);
-
-    var canvasNewWidth = w - editorContainer.clientWidth;
-
-    viewport.style.width = canvasNewWidth + 'px';
-
-    canvasStyle.width = canvasNewWidth + 'px';
-    canvasStyle.height = h + 'px';
-
-    canvas.width = canvasNewWidth * ratio;
-    canvas.height = h * ratio;
-  }
-
-  function appendResizer(editorContainer, viewportContainer) {
-
-    var resizer = document.createElement('div');
-    resizer.classList.add('resizer');
-    editorContainer.appendChild(resizer);
-
-    var editorContainerStyle = editorContainer.style;
-    var viewportContainerStyle = viewportContainer.style;
-
-    interact('.editor')
-      .resizable({
-         edges: { right: true }
-       })
-       .on('resizemove', function(event) {
-
-         if (!canvas) {
-           canvas = viewport.querySelector('canvas');
-           canvasStyle = canvas.style;
-         }
-
-         var x = (parseFloat(editorContainer.getAttribute('data-x')) || 0);
-         editorContainerStyle.width  = event.rect.width + 'px';
-
-         viewportContainerStyle.width = WIDTH - event.rect.width + 'px';
-
-         resizeViewport(WIDTH, HEIGHT);
-       });
-  }
-
   function getDefaultValue() {
 
-    return `;; Globals (initialized once): THREE, VIEWPORT, WIDTH, HEIGHT
-;; Press Alt-Enter to evaluate
-
-(def scene (THREE.Scene.))
-(def camera (THREE.PerspectiveCamera. 75 (/ WIDTH HEIGHT) 0.1 1000))
-(def renderer (THREE.WebGLRenderer. #js {"antialias" true}))
-
-(.setPixelRatio renderer js.window.devicePixelRatio)
-(.setSize renderer WIDTH HEIGHT)
-
-(.appendChild VIEWPORT renderer.domElement)
-
-(def geometry (THREE.BoxGeometry. 1 1 1))
-(def material (THREE.MeshBasicMaterial. #js {"color" 0x00ff00}))
-(def cube (THREE.Mesh. geometry material))
-
-(.add scene cube)
-
-(set! (.-z camera.position) 5)
-
-(defn animate []
-  (set! (.-x cube.rotation) (+ cube.rotation.x 0.01))
-  (set! (.-y cube.rotation) (+ cube.rotation.y 0.01)))
-
-(defn render []
-  (js/requestAnimationFrame render)
-  (animate)
-  (.render renderer scene camera))
-
-(render)`;
+    return ";; Globals (initialized once): THREE, VIEWPORT, WIDTH, HEIGHT\n;; Press Alt-Enter to evaluate\n\n(def scene (THREE.Scene.))\n(def camera (THREE.PerspectiveCamera. 75 (/ WIDTH HEIGHT) 0.1 1000))\n(def renderer (THREE.WebGLRenderer. #js {\"antialias\" true}))\n\n(.setPixelRatio renderer js.window.devicePixelRatio)\n(.setSize renderer WIDTH HEIGHT)\n\n(.appendChild VIEWPORT renderer.domElement)\n\n(def geometry (THREE.BoxGeometry. 1 1 1))\n(def material (THREE.MeshBasicMaterial. #js {\"color\" 0x00ff00}))\n(def cube (THREE.Mesh. geometry material))\n\n(.add scene cube)\n\n(set! (.-z camera.position) 5)\n\n(defn animate []\n  (set! (.-x cube.rotation) (+ cube.rotation.x 0.01))\n  (set! (.-y cube.rotation) (+ cube.rotation.y 0.01)))\n\n(defn render []\n  (js/requestAnimationFrame render)\n  (animate)\n  (.render renderer scene camera))\n\n(render)";
   }
 
 })(window);
