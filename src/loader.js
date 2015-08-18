77694fe3e5f6b90ccfd6cfa5a085c43f26d754c1
Roman Liutikov <roman01la@romanliutikov.com>
2015-08-18 22:51:25 +0300

    pretty toolbar

--- src/loader.js
+++ src/loader.js
@@ -2,20 +2,25 @@
 
   'use strict';
 
-  document.querySelector('.obj-file')
-    .addEventListener('change', readOBJ.bind(null, parseOBJ), false);
+  var objFile = document.querySelector('.obj-file');
+  var objBtn = document.querySelector('.obj-btn');
 
-function readOBJ(callback, event) {
-  var reader = new FileReader();
-  reader.addEventListener('loadend', function(ent) {
-    callback(ent.target.result);
+  objFile.addEventListener('change', readOBJ.bind(null, parseOBJ), false);
+  objBtn.addEventListener('click', function() {
+    objFile.click();
   }, false);
-  reader.readAsText(event.target.files[0]);
-}
 
-function parseOBJ(data) {
-  cljs.user.MODELS_DATA.push(data);
-  cljs.user.MODELS.push(THREE.OBJLoader.prototype.parse(data));
-}
+  function readOBJ(callback, event) {
+    var reader = new FileReader();
+    reader.addEventListener('loadend', function(ent) {
+      callback(ent.target.result);
+    }, false);
+    reader.readAsText(event.target.files[0]);
+  }
+
+  function parseOBJ(data) {
+    cljs.user.MODELS_DATA.push(data);
+    cljs.user.MODELS.push(THREE.OBJLoader.prototype.parse(data));
+  }
 
 })(window);
