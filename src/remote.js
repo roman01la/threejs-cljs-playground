8969e7da2f9d66a77ce5da0a82eaceba28df5ca2
Roman Liutikov <roman01la@romanliutikov.com>
2015-08-19 05:05:25 +0300

    ➕ sign in with GitHub & create personal demos

--- src/remote.js
+++ src/remote.js
@@ -3,12 +3,29 @@
   'use strict';
 
   var remote = new Firebase("https://cljs-three-pg.firebaseio.com/");
+  scope._user = false;
 
   scope.remote = {
 
     get: function(id) {
       return new Promise(function(resolve, reject) {
-        remote.child('sheets/' + id).once('value', function(res) {
+        var req;
+
+        if (scope._user) {
+          req = remote.child('users').child(scope._user.uid).child('sheets').child(id);
+        } else {
+          req = remote.child('sheets').child(id);
+        }
+
+        req.once('value', function(res) {
+          resolve(res.val());
+        }, function(err) { reject(err); });
+      });
+    },
+
+    getAll: function() {
+      return new Promise(function(resolve, reject) {
+        remote.child('users').child(scope._user.uid).child('sheets').once('value', function(res) {
           resolve(res.val());
         }, function(err) { reject(err); });
       });
@@ -16,18 +33,48 @@
 
     set: function(hid, code) {
       return new Promise(function(resolve, reject) {
-        var id = hid || guid();
-        remote.child('sheets/' + id).set(code, function(err) {
-          if (err) {
-            var nid = guid();
-            remote.child('sheets/' + nid).set(code, function(err) {
-              if (err) { return reject(err); }
-              resolve(nid);
-            });
-          }
+        var id = hid || guid(), req;
+
+        if (scope._user) {
+          req = remote.child('users').child(scope._user.uid).child('sheets').child(id);
+        } else {
+          req = remote.child('sheets').child(id);
+        }
+
+        req.set(code, function(err) {
+          if (err) { reject(err); }
           resolve(id);
         });
       });
+    },
+
+    signIn: function() {
+      return new Promise(function(resolve, reject) {
+        remote.authWithOAuthPopup('github', function(err, authData) {
+          if (err) { return reject(err); }
+          scope._user = authData;
+          resolve(authData);
+        });
+      });
+    },
+
+    signOut: function() {
+      scope._user = false;
+      return Promise.resolve(remote.unauth());
+    },
+
+    checkAuth: function() {
+      return new Promise(function(resolve) {
+        remote.onAuth(function check(user) {
+          remote.offAuth(check);
+          scope._user = user;
+          resolve(user);
+        });
+      });
+    },
+
+    addUser: function(uid, info) {
+      remote.child('users').child(uid).child('info').set(info);
     }
   };
 
