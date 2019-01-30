e78618177f5dbbc4eb56b6e3e846a97d0b2e5958
roman01la <roman01la@romanliutikov.com>
2019-01-30 16:15:33 +0200

    bump deps

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
 (defproject threejs-cljs-pg "1.0.0"
   :description "three.js playground in ClojureScript"
-  :dependencies [[org.clojure/clojure          "1.7.0"]
-                 [org.clojure/clojurescript    "1.7.28"]
-                 [org.clojure/core.async       "0.1.346.0-17112a-alpha"]
+  :dependencies [[org.clojure/clojure          "1.10.0"]
+                 [org.clojure/clojurescript    "1.10.439"]
+                 [org.clojure/core.async       "0.4.490"]
                  [org.omcljs/om                "0.9.0"]
                  [secretary                    "1.2.3"]
                  [cljsjs/codemirror            "5.6.0-0"]
-                 [cljsjs/three                 "0.0.70-0"]
-                 [cljsjs/firebase              "2.2.7-1"]]
+                 [cljsjs/firebase              "2.2.7-1"]
+                 [cljsjs/three                 "0.0.91-1"]]
 
-  :plugins [[lein-cljsbuild "1.0.6"]
-            [lein-figwheel  "0.3.7"]]
+  :plugins [[lein-cljsbuild "1.1.7"]
+            [lein-figwheel  "0.5.18"]]
 
   :source-paths ["src/cljs"]
   :resource-paths ["resources"]
@@ -31,7 +31,7 @@
                  :dump-core false
                  :static-fns true
                  :optimize-constants true
-                 :source-maps true
+                 :source-map true
                  :source-map-timestamp true
                  :verbose true
                  :cache-analysis true
